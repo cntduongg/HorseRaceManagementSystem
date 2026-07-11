2421e6777971810cb48a8f7c54cbb7aed2f3d8e3
longcademic <longcademic@gmail.com>
2026-07-11 22:37:22 +0900

    tournament fix

--- Application/Usecases/Tournaments/GetTournamentList/TournamentListItemResponse.cs
+++ Application/Usecases/Tournaments/GetTournamentList/TournamentListItemResponse.cs
@@ -3,6 +3,7 @@ namespace Application.Usecases.Tournaments.GetTournamentList;
 public sealed record TournamentListItemResponse(
     int TournamentId,
     string Name,
+    string? Location,
     DateOnly StartDate,
     DateOnly EndDate,
     string Status,
